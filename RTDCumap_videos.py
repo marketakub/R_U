db1e53c729fb5b1ec95eddd4197ccfe5d9b87aef
Marketa Kubankova <kubankova.marketa@gmail.com>
2019-02-20 17:46:03 +0100

    Video load newest

--- RTDCumap_videos.py
+++ RTDCumap_videos.py
@@ -5,79 +5,165 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import cv2
+import imageio
 
 
 #################################################################################################
 ######################################## load video #############################################
 
 
-def framecapture(videofilename):
+def framecapture(filename):
     
-    cap = cv2.VideoCapture(videofilename)
-    framecount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    ret, frame = cap.read()
-    w = frame.shape[0]
-    h = frame.shape[1]
-    d = frame.shape[2]
-    print(framecount, w, h, d)
-    framearray = np.empty((framecount, w, h), dtype=frame.dtype)
-    i=0
+#    cap = cv2.VideoCapture(videofilename)
+#    framecount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+#    ret, frame = cap.read()
+#    w = frame.shape[0]
+#    h = frame.shape[1]
+#    d = frame.shape[2]
+#    print(framecount, w, h, d)
+#    framearray = np.empty((framecount, w, h), dtype=frame.dtype)
+#    i=0
+#    
+#    while(cap.isOpened()):
+#        ret, frame = cap.read()
+#        if ret==True:
+#            #image processing here
+#            _, frame = cap.read()
+#            framearray[i]= frame[:, :, 0]
+#            i=i+1
+#            print(i)
+#            #img = cv2.flip(frame,1)   # flip left-right  
+#            #img = cv2.flip(img,0)     # flip up-down
+#            #cv2.imshow('frame',frame)                                              # show the video
+#            if i == 876:
+#                break
+#        else:
+#            break
+#    cap.release()
+#    cv2.destroyAllWindows()
+#    print("------------Video loaded------------------")
     
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret==True:
-            #image processing here
-            _, frame = cap.read()
-            framearray[i]= frame[:, :, 0]
-            i=i+1
-            print(i)
-            #img = cv2.flip(frame,1)   # flip left-right  
-            #img = cv2.flip(img,0)     # flip up-down
-            #cv2.imshow('frame',frame)                                              # show the video
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-    print("------------Video loaded------------------")
-    return[framearray]
+    videofilename = filename.replace(".",".avi")
+    tsvfilename = filename.replace(".",".tsv")
+    
+   
+    df = pd.read_table(tsvfilename, encoding = "ISO-8859-1")
+    df = df.drop(0)
+    
+    cap = imageio.get_reader(videofilename)
+    nr_of_images = cap.get_length()
+    pix = 0.34
+    
+    Images = []
+    for idx in range(nr_of_images):
+        cellimg = cap.get_data(idx)
+        cellimg = cellimg[:,:,0]
+        Images.append(cellimg)
+        #Images = np.array(Images)
+        
 
-frames_Bcells = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_Bcells.avi")
+
+
+    #read_csv('file', encoding = "ISO-8859-1")
+#    i = 2
+#    img = Images[i]
+#    pos_x = float(df["pos_x"].iloc[i])/pix
+#    pos_y = float(df["pos_y"].iloc[i])/pix
+#
+#    plt.imshow(img)
+#    plt.scatter(pos_x,pos_y,c='w')
+    
+    #return[framearray]
+    Images.append(df)
+    return[Images, df]
+
+
+
+
+
+
+frames_unknown, labels_unknown = framecapture(r"D:\Germany\UMAP_repo\20180211_3_45_19_unknown.")
+frames_basos_ex, labels_basos_ex = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_basos_ex.")
+frames_Bcells, labels_Bcells = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_Bcells.")
+frames_CD3neg_NK, labels_CD3neg_NK = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_CD3neg_NK.")
+frames_CD3pos_NK, labels_CD3pos_NK = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_CD3pos_NK.")
+frames_debris, labels_debris = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_debris.")
+frames_eos, labels_eos = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_eos.")
+frames_ery, labels_ery = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_ery.")
+frames_erydub, labels_erydub = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_erydub.")
+frames_lymphos_ex, labels_lymphos_ex = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_lymphos_ex.")
+frames_monos, labels_monos = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_mono.")
+frames_neutro, labels_neutro = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_neutro.")
+frames_Tcells, labels_Tcells = framecapture(r"D:\Germany\UMAP_repo\20180211_3_56_19_Tcells.")
+
+
+
+
+
+#frames_Bcells = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_Bcells.avi")
 
 
 #vfname = (r"D:\Germany\UMAP_repo\20180211_3_56_19-M1.avi")
 #loaded_frames = framecapture(videofilename = vfname)
 
 
-frames_unknown = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_45_19_unknown.avi")
-frames_basos_ex = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_basos_ex.avi")
-frames_Bcells = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_Bcells.avi")
-frames_CD3neg_NK = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_CD3neg_NK.avi")
-frames_CD3pos_NK = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_CD3pos_NK.avi")
-frames_debris = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_debris.avi")
-frames_eos = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_eos.avi")
-frames_ery = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_ery.avi")
-frames_erydub = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_erydub.avi")
-frames_lymphos_ex = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_lymphos_ex.avi")
-frames_monos = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_mono.avi")
-frames_neutro = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_neutro.avi")
-frames_Tcells = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_Tcells.avi")
+#frames_unknown = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_45_19_unknown.avi")
+#frames_basos_ex = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_basos_ex.avi")
+#frames_Bcells = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_Bcells.avi")
+#frames_CD3neg_NK = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_CD3neg_NK.avi")
+#frames_CD3pos_NK = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_CD3pos_NK.avi")
+#frames_debris = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_debris.avi")
+#frames_eos = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_eos.avi")
+#frames_ery = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_ery.avi")
+#frames_erydub = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_erydub.avi")
+#frames_lymphos_ex = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_lymphos_ex.avi")
+#frames_monos = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_mono.avi")
+#frames_neutro = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_neutro.avi")
+#frames_Tcells = framecapture(videofilename = r"D:\Germany\UMAP_repo\20180211_3_56_19_Tcells.avi")
+
 
 
 px_res = 0.34
 test_df = pd.read_csv(r'D:\Germany\UMAP_repo\20180211_3_56_19_Bcells.tsv', delimiter="\t", encoding='unicode_escape')
 test_df2 = test_df['pos_x']
 test_df2b = test_df2[1:]
-test_df2c = test_df2b/px_res
-
-print(test_df2b.dtype)
 test_df2bb = test_df2b.astype('float64')
 print(test_df2bb.dtype)
 test_df2c = test_df2bb/px_res
 
 
+Bcells=frames_Bcells[0]
+#show some images (set to RBC)
+fig, ax_array = plt.subplots(3, 3)
+axes = ax_array.flatten()
+for i, ax in enumerate(axes):
+    ax.imshow(Bcells[i,:,:], cmap='gray_r')
+plt.setp(axes, xticks=[], yticks=[], frame_on=False)
+plt.tight_layout(h_pad=0.01, w_pad=0.01)
+print("------------Images shown------------------")
+
+
 test_df3 = test_df['pos_y']
+
+
+#
+#plt.figure()
+#plt.imshow(Bcells[0,:,:])
+#plt.figure()
+#plt.imshow(Bcells[1,:,:])
+#plt.figure()
+#plt.imshow(Bcells[2,:,:])
+#plt.figure()
+#plt.imshow(Bcells[3,:,:])
+#plt.figure()
+#plt.imshow(Bcells[4,:,:])
+#plt.figure()
+#plt.imshow(Bcells[5,:,:])
+
+
+
+
+
 
 
 #################################################################################################
