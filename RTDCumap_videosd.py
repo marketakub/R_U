1145fa48a4791835ab2577139777e8e50a542a48
Marketa Kubankova <kubankova.marketa@gmail.com>
2019-02-26 22:06:07 +0100

    tidy code, plotting of continuous parameters

--- RTDCumap_videosd.py
+++ RTDCumap_videosd.py
@@ -11,8 +11,8 @@ from io import BytesIO
 from PIL import Image
 import base64
 from bokeh.plotting import figure, show, output_file
-from bokeh.models import ColumnDataSource, CategoricalColorMapper, ContinuousColorMapper, LinearColorMapper
-from bokeh.palettes import Set1, Paired, Oranges
+from bokeh.models import ColumnDataSource, CategoricalColorMapper, LinearColorMapper, ColorBar
+from bokeh.palettes import Set1, Paired, Oranges, Viridis256
 def embeddable_image(data):                                                     #create png image for bokeh
     img_data = data.astype(np.uint8)
     #image = Image.fromarray(img_data, mode='L').resize((64, 64), Image.BICUBIC)
@@ -59,22 +59,26 @@ def framecapture(filename):
         
     plt.imshow(cellimg_cropped)
     #plt.scatter(pos_x,pos_y,c='w')
-    Images=pd.DataFrame(Images)
-    Table = df
     
-    return[Images, Table]   
+    Images=pd.DataFrame(Images)
+    
+    Table = df.reset_index()
+    Table = Table.drop('level_0', axis=1)  
+    I_T_df = pd.concat([Images, Table], axis = 1)
+    
+    return[I_T_df, Images, Table]  
     
 ######################################################################################################
 ######################### embed new points using existing embedding ##################################
 ######################################################################################################
     
-def new_embedding(frames_newembedding):
+def new_embedding(trained_embedding, frames_newembedding):
     
     frames_newembedding_removednan = np.nan_to_num(frames_newembedding)
     
     #embed and time
     start = time. time()
-    embedding = embedding.transform(frames_newembedding_removednan)
+    newembedding = trained_embedding.transform(frames_newembedding_removednan)
     end = time. time()
     print("------------New UMAP embedding finished, embedding time = ", end - start, "------------------")
 
@@ -82,27 +86,40 @@ def new_embedding(frames_newembedding):
 #    one_image_one_row=frames_newembedding[0:1,:] #takes first row from many rows where 1 row = 1 image
 #    embedding = embedding.transform(one_image_one_row)
 
-    num_im=frames_newembedding.shape[0]
-    labels_newembedding_zeros = (np.zeros(num_im))
-    reshaped_frames=(frames_newembedding.values.reshape(num_im,24,34))*255+60
-    plt.imshow(reshaped_frames[0,:,:])
+    return[newembedding]
+
+######################################################################################################
+######################### Create DF with embedding, parameters, labels ###############################
+######################################################################################################    
+
+def create_df(all_embedded, all_frames, all_labels, all_tables):
     
-    newembedding_df = pd.DataFrame(embedding, columns=('x', 'y'))
-    newembedding_df['digit'] = [str(x) for x in labels_newembedding_zeros]
-    newembedding_df['image'] = list(map(embeddable_image, reshaped_frames))
-    print("------------Dataframe with new embedding created------------------")
-
-    return[newembedding_df] 
-
+    all_frames_reshaped=(all_frames.values.reshape(all_frames.shape[0],24,34))*255+60
+    #plt.imshow(all_frames_reshaped[0,:,:])    
+    em_df = pd.DataFrame(all_embedded, columns=('x', 'y'))
+    em_df['digit'] = [str(x) for x in all_labels]
+    em_df['image'] = list(map(embeddable_image, all_frames_reshaped))
+    em_df = pd.concat([em_df, all_tables], axis=1)
+    
+    return[em_df]    
 
 ######################################################################################################
-########################################## plot embedding ############################################
+################################ PLOT embedding - training data ######################################
 ######################################################################################################
 
-def plot_embedding(newembedding_df, color_mapping):
+def plot_embedding(df_embedding, title):
 
     output_file("UMAP_embedding.html")
-    datasource = ColumnDataSource(newembedding_df)
+    df_embedding = df_embedding.drop('level_0', axis=1)
+    datasource = ColumnDataSource(df_embedding)
+    
+    #mapper = CategoricalColorMapper(factors=['0', '1', '2', '3', '4' '5', '6', '7', '8'],
+    #                                   palette=Paired[9])
+    #mapper = CategoricalColorMapper(factors=['0.0'], palette=Oranges[3])
+    
+    mapper = LinearColorMapper(palette=Viridis256, low=0, high=0.1)
+    color_bar = ColorBar(color_mapper=mapper, location=(0,0), label_standoff=8, major_label_text_font_size="10pt")
+    
     tooltips1="""
     <div>
         <div>
@@ -119,7 +136,7 @@ def plot_embedding(newembedding_df, color_mapping):
     </div>
                 """
     plot_figure = figure(
-            title='UMAP projection of the dataset',
+            title=title,
             plot_width=600,
             plot_height=600,
             tools=('pan, wheel_zoom, reset'),
@@ -133,75 +150,103 @@ def plot_embedding(newembedding_df, color_mapping):
             'x',
             'y',
             source=datasource,
-            color=dict(field='digit', transform=color_mapping),
+            color=dict(field='deform', transform=mapper),   ################### change to 'digit' if we want labels
             line_alpha=0.6,
             fill_alpha=0.6,
             size=4
             )
+
+    plot_figure.add_layout(color_bar, 'right')
     show(plot_figure)
     
     return[]
-  
+
 ###########################################################################################################
 ##################################         TRAINING DATASET         #######################################
 ###########################################################################################################
     
-frames_basos_ex, table_basos_ex, = framecapture(r"E:\UMAP_repo\20180211_3_56_19_basos_ex.")
-frames_Bcells, table_Bcells = framecapture(r"E:\UMAP_repo\20180211_3_56_19_Bcells.")
-frames_CD3neg_NK, table_CD3neg_NK = framecapture(r"E:\UMAP_repo\20180211_3_56_19_CD3neg_NK.")
-frames_CD3pos_NK, table_CD3pos_NK = framecapture(r"E:\UMAP_repo\20180211_3_56_19_CD3pos_NK.")
-frames_debris, table_debris = framecapture(r"E:\UMAP_repo\20180211_3_56_19_debris.")
-frames_eos, table_eos = framecapture(r"E:\UMAP_repo\20180211_3_56_19_eos.")
-frames_ery, table_ery = framecapture(r"E:\UMAP_repo\20180211_3_56_19_ery.")
-frames_erydub, table_erydub = framecapture(r"E:\UMAP_repo\20180211_3_56_19_erydub.")
-frames_lymphos_ex, table_lymphos_ex = framecapture(r"E:\UMAP_repo\20180211_3_56_19_lymphos_ex.")
-frames_monos, table_monos = framecapture(r"E:\UMAP_repo\20180211_3_56_19_mono.")
-frames_neutro, table_neutro = framecapture(r"E:\UMAP_repo\20180211_3_56_19_neutro.")
-frames_Tcells, table_Tcells = framecapture(r"E:\UMAP_repo\20180211_3_56_19_Tcells.")
+df_basos_ex, frames_basos_ex, table_basos_ex = framecapture(r"E:\UMAP_repo\20180211_3_56_19_basos_ex.")
+df_Bcells, frames_Bcells, table_Bcells = framecapture(r"E:\UMAP_repo\20180211_3_56_19_Bcells.")
+df_CD3neg_NK, frames_CD3neg_NK, table_CD3neg_NK = framecapture(r"E:\UMAP_repo\20180211_3_56_19_CD3neg_NK.")
+df_CD3pos_NK, frames_CD3pos_NK, table_CD3pos_NK = framecapture(r"E:\UMAP_repo\20180211_3_56_19_CD3pos_NK.")
+df_debris, frames_debris, table_debris = framecapture(r"E:\UMAP_repo\20180211_3_56_19_debris.")
+df_eos, frames_eos, table_eos = framecapture(r"E:\UMAP_repo\20180211_3_56_19_eos.")
+df_ery, frames_ery, table_ery = framecapture(r"E:\UMAP_repo\20180211_3_56_19_ery.")
+df_erydub, frames_erydub, table_erydub = framecapture(r"E:\UMAP_repo\20180211_3_56_19_erydub.")
+df_lymphos_ex, frames_lymphos_ex, table_lymphos_ex = framecapture(r"E:\UMAP_repo\20180211_3_56_19_lymphos_ex.")
+df_monos, frames_monos, table_monos = framecapture(r"E:\UMAP_repo\20180211_3_56_19_mono.")
+df_neutro, frames_neutro, table_neutro = framecapture(r"E:\UMAP_repo\20180211_3_56_19_neutro.")
+df_Tcells, frames_Tcells, table_Tcells = framecapture(r"E:\UMAP_repo\20180211_3_56_19_Tcells.")
+print("------------ Training images loaded--------------")
 
 # concatenate training images to 1 matrix, concatenate corresponding labels to vector with labels
-frames = [frames_Bcells,frames_CD3neg_NK, frames_debris, frames_eos, frames_ery, frames_erydub, frames_monos, frames_neutro, frames_Tcells]
+frames = [frames_Bcells, frames_CD3neg_NK, frames_debris, frames_eos, frames_ery, frames_erydub, frames_monos, frames_neutro, frames_Tcells]
 all_frames = pd.concat(frames)
-all_frames_b= np.nan_to_num(all_frames)
-all_labels=np.concatenate((np.zeros(frames_Bcells.shape[0]), np.zeros(frames_CD3neg_NK.shape[0])+1,
+all_frames_b = np.nan_to_num(all_frames)
+
+#create and concatenate labels
+all_labels= np.concatenate((np.zeros(frames_Bcells.shape[0]), np.zeros(frames_CD3neg_NK.shape[0])+1,
                            np.zeros(frames_debris.shape[0])+2, np.zeros(frames_eos.shape[0])+3, 
                            np.zeros(frames_ery.shape[0])+4, np.zeros(frames_erydub.shape[0])+5,
                            np.zeros(frames_monos.shape[0])+6, np.zeros(frames_neutro.shape[0])+7, 
                            np.zeros(frames_Tcells.shape[0])+8 ))
+all_labels = all_labels.astype(int)
+all_labels_df = pd.DataFrame(all_labels, columns=['labels'])
 
+#concatenate tables
+tables = [table_Bcells, table_CD3neg_NK, table_debris, table_eos, table_ery, table_erydub, table_monos, table_neutro, table_Tcells]
+all_tables = pd.concat(tables)
+all_tables = all_tables.reset_index()
+
+##concatenate dataframes
+#dataframes = [df_Bcells, df_CD3neg_NK, df_debris, df_eos, df_ery, df_erydub, df_monos, df_neutro, df_Tcells]
+#all_df = pd.concat(dataframes)
+#all_df = all_df.reset_index()
+#all_df_labelled = pd.concat([all_df, all_labels_df], axis=1)
+#all_df_labelled= np.nan_to_num(all_df_labelled)
+
+###########################################
+###########################################
 # train UMAP using training data and labels    
 reducer = umap.UMAP(n_neighbors=15)
+print("------------ Embedding in progress --------------")
 start = time. time()
 embedding = reducer.fit(all_frames_b, all_labels)
-#embedding = reducer.fit_transform(all_frames_b)
+#embedding = reducer.fit_transform(all_frames)
 end = time. time()
 print("------------UMAP embedding finished, embedding time = ", (end - start), "--------------")
 
-# plot the embedding of the training dataset
-reshaped_all=(all_frames.values.reshape(all_labels.shape[0],24,34))*255+60
-plt.imshow(reshaped_all[0,:,:])
+###########################################
+###########################################
+#create dataframe of embedding
+training_embedding_df = create_df(embedding.embedding_, all_frames, all_labels, all_tables)
+training_embedding_df = training_embedding_df[0]
 
-training_embedding_df = pd.DataFrame(embedding.embedding_, columns=('x', 'y'))
-training_embedding_df['digit'] = [str(x) for x in all_labels]
-training_embedding_df['image'] = list(map(embeddable_image, reshaped_all))
-#print(bloodembedding_df['digit'].dtype)
-
-colormapping_training = CategoricalColorMapper(factors=['0.0', '1.0', '2.0', '3.0', '4.0' '5.0', '6.0', '7.0', '8.0'],
-                                       palette=Paired[9])
-plot_embedding(training_embedding_df, colormapping_training)
+#plot embedding of training data
+title_training = 'Embedding of training data'
+plot_embedding(training_embedding_df, title_training)
 print("------------Training embedding plotted-------------- ")
 
 
 ####################################################################################################################
-############################################## new embedding - B-cells #############################################
+#################################### embedding and plotting of new points ##########################################
 ####################################################################################################################
 
-frames_newembedding, labels_newembedding, IMS_newembedding = framecapture(
+#load new file, create fake labels
+df_new, frames_new, table_new = framecapture(
+#        r"E:\UMAP_repo\20180211_3_45_19_unknown.") #load
         r"E:\UMAP_repo\20190208_CD3_CD56_CD19_Bcells2.") #load
+labels_new = np.zeros(frames_new.shape[0])
 
-newembedding_df = new_embedding(frames_newembedding)
+#use trained embedding to embed new points (frames_new)
+newembedding = new_embedding(embedding, frames_new)
 
-color_mapping = CategoricalColorMapper(factors=['0.0'], palette=Oranges[3])
-plot_embedding(newembedding_df, color_mapping)
+#create dataframe with embedding, frames, labels (fake 0) and table with parameters
+df_newembedding = create_df(newembedding, frames_new, labels_new, table_new)
+
+#create plot of new embedding
+title = 'Embedding of Unknown'
+plot_embedding(df_newembedding, title)
+
 
 
