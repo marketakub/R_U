4469bb9a919db99ca3cdf82eb3fe1e60edaf96fb
Marketa Kubankova <kubankova.marketa@gmail.com>
2019-02-27 23:00:44 +0100

    UMAP
    Linked graphs, embedding of training data and new unseen data,
    pickle embedding, supervised and unsupervised embedding

--- RTDCumap_videosd.py
+++ RTDCumap_videosd.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-import cv2
+import seaborn as sns
+from sklearn import model_selection
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 import imageio
 import umap
 import time
+import pickle
+from datetime import datetime
 from io import BytesIO
 from PIL import Image
 import base64
@@ -21,6 +25,10 @@ def embeddable_image(data):                                                     
     image.save(buffer, format='png')
     for_encoding = buffer.getvalue()
     return 'data:image/png;base64,' + base64.b64encode(for_encoding).decode()
+from bokeh.io import output_file, show
+from bokeh.layouts import widgetbox, column, row, gridplot
+from bokeh.models.widgets import Select, Button
+from bokeh.models.callbacks import CustomJS
 
 ####################################################################################################
 ########################################### load video #############################################
@@ -98,27 +106,33 @@ def create_df(all_embedded, all_frames, all_labels, all_tables):
     #plt.imshow(all_frames_reshaped[0,:,:])    
     em_df = pd.DataFrame(all_embedded, columns=('x', 'y'))
     em_df['digit'] = [str(x) for x in all_labels]
+    em_df['digit2'] = em_df['digit'].map({'0':'lym_B', '1':'lym_NK', '2':'debris', '3':'eos', '4':'RBC', 
+         '5':'RBC_2', '6':'monos', '7':'neutro', '8':'lym_T', '100':'new'})
     em_df['image'] = list(map(embeddable_image, all_frames_reshaped))
     em_df = pd.concat([em_df, all_tables], axis=1)
     
     return[em_df]    
 
 ######################################################################################################
-################################ PLOT embedding - training data ######################################
+######################################### PLOT embedding #############################################
 ######################################################################################################
 
-def plot_embedding(df_embedding, title):
+def plot_embedding(df_embedding, title, df_embedding_training):
 
-    output_file("UMAP_embedding.html")
-    df_embedding = df_embedding.drop('level_0', axis=1)
+    output_file(title)
+    #df_embedding = df_embedding.drop('level_0', axis=1)
     datasource = ColumnDataSource(df_embedding)
-    
-    #mapper = CategoricalColorMapper(factors=['0', '1', '2', '3', '4' '5', '6', '7', '8'],
-    #                                   palette=Paired[9])
+    datasource2 = ColumnDataSource(df_embedding_training)
+
+    mapper1 = CategoricalColorMapper(factors=['0', '1', '2', '3', '4' '5', '6', '7', '8'],
+                                     palette=Paired[9])
     #mapper = CategoricalColorMapper(factors=['0.0'], palette=Oranges[3])
-    
-    mapper = LinearColorMapper(palette=Viridis256, low=0, high=0.1)
-    color_bar = ColorBar(color_mapper=mapper, location=(0,0), label_standoff=8, major_label_text_font_size="10pt")
+    mapper2 = LinearColorMapper(palette=Viridis256, low=0, high=0.1)
+    color_bar2 = ColorBar(color_mapper=mapper2, location=(0,0), label_standoff=8, major_label_text_font_size="10pt")
+    mapper3 = LinearColorMapper(palette=Viridis256, low=0, high=100)
+    color_bar3 = ColorBar(color_mapper=mapper3, location=(0,0), label_standoff=8, major_label_text_font_size="10pt")
+    mapper4 = LinearColorMapper(palette=Viridis256, low=10, high=40)
+    color_bar4 = ColorBar(color_mapper=mapper4, location=(0,0), label_standoff=8, major_label_text_font_size="10pt")
     
     tooltips1="""
     <div>
@@ -135,10 +149,15 @@ def plot_embedding(df_embedding, title):
         </div>             
     </div>
                 """
-    plot_figure = figure(
-            title=title,
+
+    #button = Button(label = "OK", button_type = "success")
+    #select = Select(title="Colour coding:", value="Cell types", options=["Cell types", "Deformation", "Area", "Brightness"])
+
+    plot_figure1 = figure(
+            #title=title,
+            title='Training data with labels',
             plot_width=600,
-            plot_height=600,
+            plot_height=400,
             tools=('pan, wheel_zoom, reset'),
             tooltips = tooltips1,
             #x_range=[-15, -7],
@@ -146,18 +165,97 @@ def plot_embedding(df_embedding, title):
             x_range=[-22, 15],
             y_range=[-12, 20]
             )
-    plot_figure.circle(
+    plot_figure1.circle(
+            'x',
+            'y',
+            source=datasource2,
+            color=dict(field='digit', transform=mapper1),   ################### change to 'digit' if we want labels
+            line_alpha=0.6,
+            fill_alpha=0.6,
+            size=4,
+            legend = 'digit2'
+            )
+    plot_figure1.legend.orientation = "vertical"
+    plot_figure1.legend.location = "bottom_right"
+       
+    plot_figure2 = figure(
+            #title=title,
+            title='Deformation',
+            plot_width=600,
+            plot_height=400,
+            tools=('pan, wheel_zoom, reset'),
+            tooltips = tooltips1,
+            #x_range=[-15, -7],
+            #y_range=[-1, 9]
+            x_range=plot_figure1.x_range,
+            y_range=plot_figure1.y_range
+            )
+    plot_figure2.circle(
             'x',
             'y',
             source=datasource,
-            color=dict(field='deform', transform=mapper),   ################### change to 'digit' if we want labels
+            color=dict(field='deform', transform=mapper2),   ################### change to 'digit' if we want labels
             line_alpha=0.6,
             fill_alpha=0.6,
             size=4
             )
+    plot_figure2.add_layout(color_bar2, 'right')
+    
+    plot_figure3 = figure(
+            title='Area',
+            plot_width=600,
+            plot_height=400,
+            tools=('pan, wheel_zoom, reset'),
+            tooltips = tooltips1,
+            #x_range=[-15, -7],
+            #y_range=[-1, 9]
+            x_range=plot_figure1.x_range,
+            y_range=plot_figure1.y_range
+            )
+    plot_figure3.circle(
+            'x',
+            'y',
+            source=datasource,
+            color=dict(field='area_um', transform=mapper3),   ################### change to 'digit' if we want labels
+            line_alpha=0.6,
+            fill_alpha=0.6,
+            size=4
+            )
+    plot_figure3.add_layout(color_bar3, 'right')
 
-    plot_figure.add_layout(color_bar, 'right')
-    show(plot_figure)
+    plot_figure4 = figure(
+            title='Brightness',
+            plot_width=600,
+            plot_height=400,
+            tools=('pan, wheel_zoom, reset'),
+            tooltips = tooltips1,
+            #x_range=[-15, -7],
+            #y_range=[-1, 9]
+            x_range=plot_figure1.x_range,
+            y_range=plot_figure1.y_range
+            )
+    plot_figure4.circle(
+            'x',
+            'y',
+            source=datasource,
+            color=dict(field='bright_avg', transform=mapper4),   ################### change to 'digit' if we want labels
+            line_alpha=0.6,
+            fill_alpha=0.6,
+            size=4
+            )
+    plot_figure4.add_layout(color_bar4, 'right')
+    
+#    callback_function = CustomJS(args=dict(source=datasource), code="""
+#                        var data = source.data;
+#                        var f = cb_obj.value
+#                    """)
+    
+    grid = gridplot([[plot_figure1, plot_figure2], [plot_figure3, plot_figure4]])
+    show(grid)
+    
+    #layout = column(widgetbox(select), widgetbox(button), plot_figure)
+    #layout = row(plot_figure, plot_figure2)
+    #show(layout)
     
     return[]
 
@@ -209,22 +307,46 @@ all_tables = all_tables.reset_index()
 ###########################################
 # train UMAP using training data and labels    
 reducer = umap.UMAP(n_neighbors=15)
-print("------------ Embedding in progress --------------")
+
+#supervised UMAP
+print("------------ Embedding in progress (supervised) --------------")
 start = time. time()
 embedding = reducer.fit(all_frames_b, all_labels)
-#embedding = reducer.fit_transform(all_frames)
 end = time. time()
-print("------------UMAP embedding finished, embedding time = ", (end - start), "--------------")
+print("------------UMAP embedding (supervised) finished, embedding time = ",
+      (end - start), "--------------")
+
+#unsupervised UMAP
+print("------------ Embedding in progress (unsupervised) --------------")
+start = time. time()
+embedding2 = reducer.fit(all_frames_b)
+end = time. time()
+print("------------ UMAP embedding (unsupervised) finished, embedding time = ",
+      (end - start), "--------------")
+
+#saves reducer (the model to be used for future embeddings) to disk with todays date:
+date_string = datetime.strftime(datetime.date(datetime.now()), '%Y%m%d')
+embedding_model_filename = (date_string + "_embedding_1.sav")
+pickle.dump(embedding, open(embedding_model_filename, 'wb'))
 
 ###########################################
 ###########################################
-#create dataframe of embedding
+#create dataframe of embedding - supervised
 training_embedding_df = create_df(embedding.embedding_, all_frames, all_labels, all_tables)
 training_embedding_df = training_embedding_df[0]
+training_embedding_df = training_embedding_df.drop('level_0', axis=1) ##used to be in plotting funciton at line 4
+#plot
+title_training = "Embedding_of_training_data.html"
+plot_embedding(training_embedding_df, title_training, training_embedding_df)
+print("------------Training embedding plotted-------------- ")
 
-#plot embedding of training data
-title_training = 'Embedding of training data'
-plot_embedding(training_embedding_df, title_training)
+#create dataframe of embedding - unsupervised
+training_embedding_df2 = create_df(embedding2.embedding_, all_frames, all_labels, all_tables)
+training_embedding_df2 = training_embedding_df2[0]
+training_embedding_df2 = training_embedding_df2.drop('level_0', axis=1) ##used to be in plotting funciton at line 4
+#plot
+title_training2 = "Embedding_of_training_data_unsupervised.html"
+plot_embedding(training_embedding_df2, title_training2, training_embedding_df2)
 print("------------Training embedding plotted-------------- ")
 
 
@@ -236,17 +358,33 @@ print("------------Training embedding plotted-------------- ")
 df_new, frames_new, table_new = framecapture(
 #        r"E:\UMAP_repo\20180211_3_45_19_unknown.") #load
         r"E:\UMAP_repo\20190208_CD3_CD56_CD19_Bcells2.") #load
-labels_new = np.zeros(frames_new.shape[0])
+labels_new = np.zeros(frames_new.shape[0])+100
+
+#loads reducer
+loaded_embedding = pickle.load(open("20190227_embedding_1.sav", 'rb'))
 
 #use trained embedding to embed new points (frames_new)
-newembedding = new_embedding(embedding, frames_new)
+newembedding = new_embedding(loaded_embedding, frames_new)             ####### here "load_embedding", or use newly trained "embedding"
 
 #create dataframe with embedding, frames, labels (fake 0) and table with parameters
-df_newembedding = create_df(newembedding, frames_new, labels_new, table_new)
+df_newembedding = create_df(newembedding[0], frames_new, labels_new, table_new)
 
 #create plot of new embedding
-title = 'Embedding of Unknown'
-plot_embedding(df_newembedding, title)
+title = "B_cells.html"
+plot_embedding(df_newembedding[0], title, training_embedding_df)
+
+
+####################################################################################################################
+#################################### split training + testing data ##########################################
+####################################################################################################################
+#X = array[:,0:8]
+#Y = array[:,8]
+#test_size = 0.33
+#seed = 7
+#X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
+## Fit the model on 33%
+#model = LogisticRegression()
+#model.fit(X_train, Y_train)
 
 
 
